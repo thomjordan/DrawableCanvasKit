75c401ac0517c49b489e482eec6fbb4ac6a0e6c3
ThomJordan <thomjordan@users.noreply.github.com>
2018-04-10 22:47:18 -0400

    Initial Commit

---
+++ DrawableCanvasKit/DrawableCanvasKit.h
@@ -0,0 +1,19 @@
+//
+//  DrawableCanvasKit.h
+//  DrawableCanvasKit
+//
+//  Created by Thom Jordan on 4/10/18.
+//  Copyright © 2018 Thom Jordan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DrawableCanvasKit.
+FOUNDATION_EXPORT double DrawableCanvasKitVersionNumber;
+
+//! Project version string for DrawableCanvasKit.
+FOUNDATION_EXPORT const unsigned char DrawableCanvasKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawableCanvasKit/PublicHeader.h>
+
+
